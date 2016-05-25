1b05f3c91e215c089485d500bdf28bf1249dd863
Arunkumar Karunakaran <arun.kumar@dreamorbit.com>
2016-05-25 13:30:45 +0530

    Updated DB

--- Database/Hermes.Permission.Creation.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO [dbo].[Permission] ([PermissionId], [Name]) VALUES (-4, 'Delete')
-INSERT INTO [dbo].[Permission] ([PermissionId], [Name]) VALUES (-3, 'Update')
-INSERT INTO [dbo].[Permission] ([PermissionId], [Name]) VALUES (-2, 'Read')
-INSERT INTO [dbo].[Permission] ([PermissionId], [Name]) VALUES (-1, 'Create')
