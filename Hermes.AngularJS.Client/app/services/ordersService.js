2102e9291e01e696d333fab413e97ea5f60f21e3
Arunkumar Karunakaran <arun.kumar@dreamorbit.com>
2016-05-26 18:47:55 +0530

    corrected the route

--- Hermes.AngularJS.Client/app/services/ordersService.js
+++ Hermes.AngularJS.Client/app/services/ordersService.js
@@ -7,7 +7,7 @@ app.factory('ordersService', ['$http', 'ngAuthSettings', function ($http, ngAuth
 
     var _getOrders = function () {
 
-        return $http.get(serviceBase + 'api/orders').then(function (results) {
+        return $http.get(serviceBase + 'api/orders/all').then(function (results) {
             return results;
         });
     };
