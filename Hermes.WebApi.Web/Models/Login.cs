8181977fba801972d9072ce8801f6e018443ca85
Arunkumar Karunakaran <arun.kumar@dreamorbit.com>
2016-05-30 13:00:39 +0530

    tabified

--- Hermes.WebApi.Web/Models/Login.cs
+++ Hermes.WebApi.Web/Models/Login.cs
@@ -14,28 +14,28 @@
 
 namespace Hermes.WebApi.Web.Models
 {
-	/// <summary>
-	/// Class Login object.
-	/// </summary>
-	public class Login
-	{
-		/// <summary>
-		/// Gets or sets the username.
-		/// </summary>
-		/// <value>The username.</value>
-		public string Username { get; set; }
+    /// <summary>
+    /// Class Login object.
+    /// </summary>
+    public class Login
+    {
+        /// <summary>
+        /// Gets or sets the username.
+        /// </summary>
+        /// <value>The username.</value>
+        public string Username { get; set; }
 
-		/// <summary>
-		/// Gets or sets the password.
-		/// </summary>
-		/// <value>The password.</value>
-		public string Password { get; set; }
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>The password.</value>
+        public string Password { get; set; }
 
-		/// <summary>
-		/// Gets or sets a value indicating whether [remember me].
-		/// </summary>
-		/// <value><c>true</c> if [remember me]; otherwise, <c>false</c>.</value>
-		public bool RememberMe { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether [remember me].
+        /// </summary>
+        /// <value><c>true</c> if [remember me]; otherwise, <c>false</c>.</value>
+        public bool RememberMe { get; set; }
 
         /// <summary>
         /// Gets or sets the client identifier.
